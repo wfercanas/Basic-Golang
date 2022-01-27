84cad2eae8367e2c68c32e46a035cc8d41181d7c
wfercanas <fernando.canas@outlook.com>
2022-01-26 22:35:00 -0500

    add/ fmt exercises

--- src/main.go
+++ src/main.go
@@ -43,10 +43,35 @@ func arithmetic() {
 	fmt.Println("mod:", mod)
 }
 
+func fmtPackage() {
+	// Println
+	hello := "Hello"
+	world := "World"
+	fmt.Println(hello, world)
+	fmt.Println(hello, world)
+	
+	// Printf
+	nombre := "Platzi"
+	cursos := 500
+	fmt.Printf("%s tiene más de %d cursos\n", nombre, cursos)
+	fmt.Printf("%v tiene más de %v cursos\n", nombre, cursos)
+	
+	// Sprintf
+	message := fmt.Sprintf("Si!, %s tiene más de %d cursos", nombre, cursos)
+	fmt.Println(message)
+	
+	// Data Type
+	fmt.Printf("Hello: %T\n", hello)
+	fmt.Printf("Cursos: %T", cursos)
+}
+
 func main() {
 	// Class: const, var and zero-values
 	// constAndVar()
 
 	// Class: Arithmetic
-	arithmetic()
+	// arithmetic()
+
+	// Class: fmt
+	fmtPackage()
 }
