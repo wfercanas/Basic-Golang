8e7c30554ca577886735c9374d81329923cdae08
wfercanas <fernando.canas@outlook.com>
2022-01-27 11:16:50 -0500

    add/ switch conditional

--- src/main.go
+++ src/main.go
@@ -137,6 +137,27 @@ func conditionals() {
 	}
 }
 
+func switchConditional() {
+	modulo := 4 % 2
+	switch modulo {
+		case 0:
+			fmt.Println("Es par")
+		default:
+			fmt.Println("Es impar")
+	}
+	
+	// Sin condicion
+	value := 200 
+	switch {
+	case value > 100:
+		fmt.Println("Es mayor a 100")
+	case value < 0:
+		fmt.Println("Es negativo")
+	default:
+		fmt.Print("Está entre cero y 100")
+	}
+}
+
 func main() {
 	// Class: const, var and zero-values
 	// constAndVar()
@@ -154,5 +175,7 @@ func main() {
 	// forCycle()
 
 	// Conditionals
-	conditionals()
+	// conditionals()
+	switchConditional()
+	
 }
