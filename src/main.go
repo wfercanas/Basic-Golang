300c20f2e1bc0eab3d5f68e9460ac56907571465
wfercanas <fernando.canas@outlook.com>
2022-01-27 09:25:51 -0500

    add/ functions class

--- src/main.go
+++ src/main.go
@@ -65,6 +65,33 @@ func fmtPackage() {
 	fmt.Printf("Cursos: %T", cursos)
 }
 
+func printFunction(text string) {
+	fmt.Println(text)
+}
+
+func tripleParameters(a, b int, c string) {
+	fmt.Println(a, b, c)
+}
+
+func withReturn(a int) int {
+	return a * 2
+}
+
+func multipleReturn(a int) (c, d int) {
+	return a*2, a*3
+}
+
+func functions() {
+	printFunction("Hola mundo")
+	printFunction("New string")
+	tripleParameters(1, 2, "hola")
+	fmt.Println("Single return:", withReturn(3))
+	value1, value2 := multipleReturn(2)
+	onlyValue1, _ := multipleReturn(3)
+	fmt.Println("Multiple return:", value1, value2)
+	fmt.Println("Only value:", onlyValue1)
+}
+
 func main() {
 	// Class: const, var and zero-values
 	// constAndVar()
@@ -73,5 +100,8 @@ func main() {
 	// arithmetic()
 
 	// Class: fmt
-	fmtPackage()
+	// fmtPackage()
+
+	// Class: Functions
+	functions()
 }
